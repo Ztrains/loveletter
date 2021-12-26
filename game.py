14d96db39af359b8eca6bd34ad9e31bf441589c9
Ztrains <zachary.t.rich@gmail.com>
2021-12-26 00:22:12 -0500

    add basic logic for each card, and new fields for player class

--- game.py
+++ game.py
@@ -4,11 +4,15 @@ from player import Player
 
 class Game:
 
-    # for testing, remove later
-    deck = Card.newDeck()
-    Card.printDeck(deck)
+    def __init__(self) -> None:
+        self.deck = Card.newDeck()
+        Card.printDeck(self.deck)
+        
+    def drawCard(self) -> Card:
+        return self.deck.pop()
 
-
+    # is it even worth having this function? (probably not)
+    # could just check the card from gameserver when played and call appropriate func from there
     def playCard(card: Card, player: Player, target: Player):
         if (card.power == 1):
             Game.playGuard(player, target)
@@ -37,28 +41,62 @@ class Game:
     def playGuard(player: Player, target: Player):
         print('Guard played by ', player, 'against ', target)
 
-    def playPriest(player: Player, target: Player):
+        # TODO: add gameserver logic to prompt player to guess card of target
+        guess = '???'
+        
+        if (target.getCard().name == guess):
+            target.isAlive = False
+            # probably need more here
+        else:
+            pass
+
+
+    def playPriest(player: Player, target: Player) -> Card: 
         print('Priest played by ', player, 'against ', target)
+
+        # need to cast to Card type maybe?
+        return target.getCard()
+        
 
     def playBaron(player: Player, target: Player):
         print('Baron played by ', player, 'against ', target)
+
+        # need logic for removing baron before comparison
+        return player.getCard() > target.getCard()
+
 
     def playHandmaid(player: Player, target: Player):
         print('Handmaid played by ', player, 'against ', target)
         # add snarky comment if you play handmaid 1st in the round
 
+        # need to add logic in gameserver to set isProtected to False on player turn start
+        player.isProtected = True
+
+
     def playPrince(player: Player, target: Player):
         print('Prince played by ', player, 'against ', target)
-        # add logic where countess must be played if king/prince in hand
+
+        discardedCard = target.hand.pop()
+        # add logic to share discarded card with gameserver for all to see
+        target.hand.append(Game.drawCard())
+
 
     def playKing(player: Player, target: Player):
         print('King played by ', player, 'against ', target)
-        # add logic where countess must be played if king/prince in hand
+        
+        # add logic to remove king from hand before trade
+        tradedCard = target.hand.pop()
+        target.hand.append(player.hand.pop())
+        player.hand.append(tradedCard)
+
 
     def playCountess(player: Player, target: Player):
         print('Countess played by ', player, 'against ', target)
+
         # add logic where countess must be played if king/prince in hand
+        pass
 
     def playPrincess(player: Player, target: Player):
         # Not sure why you'd want to play the princess...
         print('Princess played by ', player, 'against ', target)
+        player.isAlive = False
--- player.py
+++ player.py
@@ -1,12 +1,21 @@
 
 
+from typing import List
+from card import Card
+
+
 class Player:
 
     def __init__(self, name: str) -> None:
-        self.isAlive = True
-        self.hand = []
-        self.wins = 0
+        self.isAlive: bool = True
+        self.isProtected: bool = False  # for handmaid protection
+        self.isTurn: bool = False
+        self.hand: List[Card] = []
+        self.wins: int = 0
         self.name = name
+
+    def getCard(self) -> Card:
+        return self.hand[0]
 
     @property
     def isAlive(self):
@@ -16,11 +25,30 @@ class Player:
     def isAlive(self, value: bool):
         self.isAlive = value
 
+
+    @property
+    def isProtected(self):
+        return self.isProtected
+
+    @isProtected.setter
+    def isProtected(self, value: bool):
+        self.isProtected = value
+
+
+    @property
+    def isTurn(self):
+        return self.isTurn
+
+    @isTurn.setter
+    def isTurn(self, value: bool):
+        self.isTurn = value
+
     
     @property
-    def hand(self):
+    def hand(self) -> List[Card]:
         return self.hand
 
+    # probably not even needed
     @hand.setter
     def hand(self, value):
         self.hand = value
